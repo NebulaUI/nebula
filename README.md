4d920861703b8e6946b30e7a498c961205063bc7
rbrtsmith <rbrtsmith84@googlemail.com>
2017-08-03 13:25:25 +0100

    setup docs

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# nebula
+# Nebula
+
+Documentation for the Nebula framework
+* [Nebula CSS](https://github.com/rbrtsmith/nebula-css)
+* [Nebula Components](https://github.com/rbrtsmith/nebula-css)
+* [Nebula React](https://github.com/rbrtsmith/nebula-css)
+
+## Get started
+Install one of the following.
+
+Low-level CSS (6.5kb!)
+`yarn add nebula-css`
+
+Low-level CSS + CSS Components
+`yarn add nebula-components`
+
+Low-level CSS + CSS Components + React Components
+`yarn add nebula-react`
---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+
+import {
+  SiteWrap,
+  Section,
+  Tabs,
+  BareList,
+  InlineList,
+  MatrixList,
+  UniformedList,
+} from 'nebula-react'
+
+const App = () => (
+  <div>
+    <Section size="md">
+      <SiteWrap padding>
+        <h1>Nebula React</h1>
+        <h2>Lists</h2>
+        <Tabs.Wrapper>
+          <Tabs.TabList>
+            <Tabs.Tab>Bare list</Tabs.Tab>
+            <Tabs.Tab>Inline list</Tabs.Tab>
+            <Tabs.Tab>Matrix list</Tabs.Tab>
+            <Tabs.Tab>Uniformed list</Tabs.Tab>
+          </Tabs.TabList>
+          <Tabs.Panels>
+            <Tabs.Panel>
+              <BareList.Wrapper spacing="md">
+                <BareList.Item>Help</BareList.Item>
+                <BareList.Item>Me</BareList.Item>
+              </BareList.Wrapper>
+            </Tabs.Panel>
+            <Tabs.Panel>
+              <InlineList.Wrapper spacing="md@md">
+                <InlineList.Item>Help</InlineList.Item>
+                <InlineList.Item>Me</InlineList.Item>
+              </InlineList.Wrapper>
+            </Tabs.Panel>
+            <Tabs.Panel>
+              <MatrixList.Wrapper spacing="md">
+                <MatrixList.Item>Help</MatrixList.Item>
+                <MatrixList.Item>Me</MatrixList.Item>
+              </MatrixList.Wrapper>
+            </Tabs.Panel>
+            <Tabs.Panel>
+              <UniformedList.Wrapper breakpoint="md">
+                <UniformedList.Item>Help</UniformedList.Item>
+                <UniformedList.Item>Me</UniformedList.Item>
+              </UniformedList.Wrapper>
+            </Tabs.Panel>
+          </Tabs.Panels>
+        </Tabs.Wrapper>
+      </SiteWrap>
+    </Section>
+  </div>
+)
+
+export default App
