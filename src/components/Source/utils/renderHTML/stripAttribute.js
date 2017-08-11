e523cff43afd8dfeae4aaf42e274b8c73239b356
Robert Smith <rbrtsmith84@googlemail.com>
2017-08-12 00:46:23 +0100

    refactor stripAttributes

--- src/components/Source/utils/renderHTML/stripAttribute.js
+++ src/components/Source/utils/renderHTML/stripAttribute.js
@@ -1,6 +1,4 @@
 const stripAttribute = (html, attr) =>
-  html
-    .replace(new RegExp(`${attr}=(["'])(?:(?=(\\\\?))\\2.)*?\\1`, 'g'), '')
+  html.replace(new RegExp(`${attr}="[-a-zA-Z0-9]*"`, 'g'), '')
     .replace(/\s{1,}>/g, '>')
-
 export default stripAttribute
