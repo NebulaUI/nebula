fbbb4c364abb137821b5a45f90049c6e569d1ecb
Robert Smith <rbrtsmith84@googlemail.com>
2017-08-12 10:47:44 +0100

    Test CheckboxWrapper

---
+++ src/components/ComponentExample/Options/__tests__/CheckboxWrapper.jsx
@@ -0,0 +1,97 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import { BareList, Form } from 'nebula-react'
+import shortid from 'shortid'
+
+import CheckboxWrapper from '../CheckboxWrapper'
+
+jest.mock('shortid')
+
+beforeEach(() => {
+  shortid.mockImplementation(() => 'test-id')
+})
+
+describe('<CheckboxWrapper/>', () => {
+  it('renders <BareList.Item />, <Form.CheckboxWrapper />, <Form.CheckboxInput /> and <Form.CheckboxLabel />', () => {
+    const $ = shallow(
+      <CheckboxWrapper stateKey="t" optionState={{}} handleChange={jest.fn()}>
+        _
+      </CheckboxWrapper>
+    )
+    expect($.find(BareList.Item)).toHaveLength(1)
+    expect($.find(Form.CheckboxWrapper).childAt(0).type()).toBe(Form.CheckboxInput)
+    expect($.find(Form.CheckboxWrapper).childAt(1).type()).toBe(Form.CheckboxLabel)
+  })
+
+  it('renders children inside <Form.CheckboxLabel />', () => {
+    const $ = shallow(
+      <CheckboxWrapper stateKey="t" optionState={{}} handleChange={jest.fn()}>
+        test
+      </CheckboxWrapper>
+    )
+    expect($.find(Form.CheckboxLabel).contains('test')).toBe(true)
+  })
+
+  it('takes a node to override <BareList.Item /> node type', () => {
+    const $ = shallow(
+      <CheckboxWrapper node="div" stateKey="t" optionState={{}} handleChange={jest.fn()}>
+        _
+      </CheckboxWrapper>
+    )
+    expect($.find(BareList.Item).prop('node')).toBe('div')
+  })
+
+  it('takes a stateKey that is passed as a data-attribute to <Form.CheckboxInput />', () => {
+    const $ = shallow(
+      <CheckboxWrapper stateKey="test" optionState={{}} handleChange={jest.fn()}>
+        _
+      </CheckboxWrapper>
+    )
+    expect($.find(Form.CheckboxInput).prop('data-stateKey')).toBe('test')
+  })
+
+  it('handles changes', () => {
+    const handleChange = jest.fn()
+    const $ = shallow(
+      <CheckboxWrapper stateKey="t" optionState={{}} handleChange={handleChange}>
+        _
+      </CheckboxWrapper>
+    )
+    expect(handleChange).not.toBeCalled()
+
+    $.find(Form.CheckboxInput).simulate('change')
+    expect(handleChange).toBeCalled()
+  })
+
+  it('is checked determined by the state object passed in with corresponding stateKey', () => {
+    const state = {
+      testKey1: true,
+      testKey2: {
+        nestedKey: false
+      }
+    }
+    const $ = shallow(
+      <CheckboxWrapper stateKey="testKey1" optionState={state} handleChange={jest.fn()}>
+        _
+      </CheckboxWrapper>
+    )
+    expect($.find(Form.CheckboxInput).prop('checked')).toBe(true)
+
+    const $$ = shallow(
+      <CheckboxWrapper stateKey="testKey2.nestedKey" optionState={state} handleChange={jest.fn()}>
+        _
+      </CheckboxWrapper>
+    )
+    expect($$.find(Form.CheckboxInput).prop('checked')).toBe(false)
+  })
+
+  it('auto generates an ID', () => {
+    const $ = shallow(
+      <CheckboxWrapper stateKey="t" optionState={{}} handleChange={jest.fn()}>
+        _
+      </CheckboxWrapper>
+    )
+    expect($.find(Form.CheckboxInput).prop('id')).toBe('test-id')
+    expect($.find(Form.CheckboxLabel).prop('htmlFor')).toBe('test-id')
+  })
+})
--- src/components/Source/utils/renderHTML/stripAttribute.js
+++ src/components/Source/utils/renderHTML/stripAttribute.js
@@ -1,4 +1,5 @@
 const stripAttribute = (html, attr) =>
   html.replace(new RegExp(`${attr}="[-a-zA-Z0-9]*"`, 'g'), '')
     .replace(/\s{1,}>/g, '>')
+
 export default stripAttribute
