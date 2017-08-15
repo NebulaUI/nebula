5e12255eb40ce51effee9fa7815d87bf291f7f21
rbrtsmith <rbrtsmith84@googlemail.com>
2017-08-15 09:07:45 +0100

    Complete tests for Navigation

---
+++ src/modules/App/Navigation/__tests__/renderRoutes.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import { Navbar } from 'nebula-react'
+
+import renderRoutes from '../renderRoutes'
+
+describe('renderRoutes', () => {
+  it('renders <Navbar.Dropdown.Wrapper /> if the route has descendants', () => {
+    const routes = [{
+      label: 'T',
+      to: '/',
+      descendants: [{
+        'to': '/T',
+        'label': 'T'
+      }]
+    }]
+    const $ = shallow(
+      <div>
+        { renderRoutes(routes, '/')}
+      </div>
+    )
+    expect($.childAt(0).type()).toBe(Navbar.Dropdown.Wrapper)
+  })
+
+  it('renders <Navbar.Item /> if the route has no descendants', () => {
+    const routes = [{
+      label: 'T',
+      to: '/'
+    }]
+    const $ = shallow(
+      <div>
+        { renderRoutes(routes, '/')}
+      </div>
+    )
+    expect($.childAt(0).type()).toBe(Navbar.Item)
+  })
+})
