3c44f3d8974b3f640510e769bf4a226c28235e47
Robert Smith <rbrtsmith84@googlemail.com>
2017-09-27 19:26:15 +0100

    Fix margin bug in navbar

--- src/modules/App/Navigation/SecondaryContent.jsx
+++ src/modules/App/Navigation/SecondaryContent.jsx
@@ -5,11 +5,11 @@ import { Navbar } from 'nebula-react'
 import { BASE_PATH } from 'constants/index'
 
 const SecondayContent = (
-  <Navbar.Item tag="div" resetLineHeight>
+  <Navbar.Item tag="div" resetLineHeight className="u-push-left-md">
     <NavLink
       activeStyle={{ display: 'none' }}
       to={`${BASE_PATH}get-started`}
-      className="c-btn c-btn--sm c-btn--alpha u-push-left-md"
+      className="c-btn c-btn--sm c-btn--alpha"
     >
       <span role="img" aria-label="star" style={{ color: 'transparent', textShadow: '0 0 0 white', marginRight: '0.5rem' }}>💫</span>Get started!
     </NavLink>
