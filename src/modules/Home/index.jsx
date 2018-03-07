bd4bd4f0adf85d2bc1267cae2501c85406fef80b
teodora-n <t.nikolaeva84@gmail.com>
2018-03-07 14:03:32 +0000

    Single quites - attempt 3

--- src/modules/Home/index.jsx
+++ src/modules/Home/index.jsx
@@ -13,16 +13,16 @@ import belowLayers from './belowLayers.md'
 const Home = () => (
   <Section size="md">
     <SiteWrap padding>
-      <h1><img src={`/src/assets/img/nebula-logo.svg`} alt={`Nebula`} style={{ maxWidth: '150px', height: 'auto', verticalAlign: 'middle' }} /></h1>
+      <h1><img src={'/src/assets/img/nebula-logo.svg'} alt={'Nebula'} style={{ maxWidth: '150px', height: 'auto', verticalAlign: 'middle' }} /></h1>
       <Card.Wrapper className="u-nebula-bg">
         <Card.Body>
           <MarkdownContainer source={aboveLayers} />
           <p>
-            <img src={layers} alt={`Nebula Layers`} style={{ width: '500px' }} />
+            <img src={layers} alt={'Nebula Layers'} style={{ width: '500px' }} />
           </p>
           <MarkdownContainer source={belowLayers} />
           <Link to={`${BASE_PATH}get-started`} className="c-btn c-btn--md c-btn--alpha">
-            <img src={`/src/assets/icons/nebula-icon.svg`} alt={`Nebula`} style={{ marginRight: '0.7rem', width: '25px', height: 'auto', verticalAlign: 'middle' }} />
+            <img src={'/src/assets/icons/nebula-icon.svg'} alt={'Nebula'} style={{ marginRight: '0.7rem', width: '25px', height: 'auto', verticalAlign: 'middle' }} />
             <span style={{ verticalAlign: 'middle' }}>Get started!</span>
           </Link>
         </Card.Body>
